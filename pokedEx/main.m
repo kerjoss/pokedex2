410ac9c966b05390a8acdbe22e0307485c9bc2e8
kerjoss <kerjoss@kerjossdeMacBook-Pro.local>
2017-09-24 14:06:26 +0800

    Initial Commit

---
+++ pokedEx/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pokedEx
+//
+//  Created by kerjoss on 2017/9/24.
+//  Copyright © 2017年 kerjoss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
